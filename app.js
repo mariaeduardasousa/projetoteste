adae15845f10576a754ef2d3d5d3248eb5bbcf56
Maria Eduarda <m.eduardasousa98@gmail.com>
2021-04-20 23:13:28 -0300

    Versão 1.0

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require("./models/Artigo");
+const Artigo = mongoose.model('artigo');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    //console.log("Acessou o Middleware!");
+    res.header("Acess-Control-Allow-Origin", "*");
+    res.header("Acess-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+mongoose.connect('mongodb://localhost/celke', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Conexão com o MongoDB realizada com sucesso!");
+}).catch((erro) => {
+    console.log("ERRO: Conezão com o MongoDB não foi realizada com sucesso!");
+});
+
+app.get("/artigo", (req, res) => {
+    Artigo.find({}).then((artigo) => {
+        return res.json(artigo);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        })
+    })
+});
+
+app.get("/artigo/:id", (req, res) => {
+    console.log(req.params.id);
+
+    Artigo.findOne({ _id: req.params.id }).then((artigo) => {
+        return res.json(artigo);
+    }).catch((erro) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado!"
+        })
+    })
+})
+
+app.post("/artigo", (req, res) => {
+    const artigo = Artigo.create(req.body, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Error: Artigo não foi cadastrado com sucesso!"
+        });
+        return res.status(200).json({
+            error: false,
+            message: "Artigo foi cadastrado com sucesso!"
+        })
+    });
+});
+
+app.put("/artigo/:id", (req, res) => {
+    const artigo = Artigo.updateOne({ _id: req.params.id }, req.body, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Erro artigo não foi editado com sucesso!"
+        });
+
+        return res.json({
+            error: false,
+            message: "Artigo editado com sucesso!"
+        });
+    });
+});
+
+app.delete("/artigo/:id", (req, res) => {
+    const artigo = Artigo.deleteOne({ _id: req.params.id }, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Error: Artigo não foi apagado!"
+        });
+
+        return res.json({
+            error: false,
+            message: "Artigo apagado com sucesso!"
+        });
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080/")
+});
